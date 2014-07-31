788d43b6f64077a77f978c58f34277701e981a1a
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2014-07-31 16:27:09 -0300

    Added better logger

--- examples/crud/app.js
+++ examples/crud/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    var log = Picasso.load("utils.log");
+    log.setLogLevel(log.lvs.INFO);
+
     var user = new UserModel();
     var userCreateView = new UserCreateView();
     var userListView = new UserListView();
