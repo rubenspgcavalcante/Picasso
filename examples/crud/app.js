e8022898d28f0c1a58a5102a2112ba5667a95fb4
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2014-09-25 22:56:26 -0300

    Bower package

--- examples/crud/app.js
+++ examples/crud/app.js
@@ -1,9 +1,8 @@
-var user;
 $(document).ready(function(){
     var log = Picasso.load("utils.log");
     log.setLogLevel(log.lvs.INFO);
 
-    user = new UserModel();
+    var user = new UserModel();
     var userCreateView = new UserCreateView();
     var userListView = new UserListView();
     var userCRUDController = new CRUDController(user, userListView, userCreateView);
